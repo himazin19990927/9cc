6d3ad00a55ac9a23399ba1f6c69a3c7a71f31bf3
Himazin <jfiglk@gmail.com>
2021-04-04 02:27:40 +0000

    四則演算のできる言語の作成

--- src/9cc.c
+++ src/9cc.c
@@ -276,30 +276,22 @@ int main(int argc, char **argv) {
     }
 
     // トークナイズする
-    token = tokenize(argv[1]);
-
-
-    //入力プログラムを保存
     user_input = argv[1];
+    token = tokenize(user_input);
+    Node *node = expr();
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないのでそれをチェックして
-    //最初のmov命令を出力
-    printf("  mov rax, %d\n", expect_number());
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
+    // 抽象構文木を下りながらコード生成
+    gen(node);
 
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-
+    // スタックトップに式全体の値が残っているはずなので
+    // それをRAXにロードして関数からの返り値とする
+    printf("  pop rax\n");
     printf("  ret\n");
+
     return 0;
 }
