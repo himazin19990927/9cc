8d6e1b0b9ff3d2038793902fef74681c9dd5aab3
Taiki Harada <haradataiki19990927@gmail.com>
2021-04-05 15:58:01 +0900

    ファイルの分割

---
+++ src/9cc.h
@@ -0,0 +1,80 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// parse.c
+
+typedef enum {
+    TK_RESERVED,  // 記号
+    TK_NUM,       // 整数トークン
+    TK_EOF,       // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+    TokenKind kind;  // トークンの型
+    Token *next;     // 次の入力トークン
+    int val;         // kindがTK_NUMの場合、その数値
+    char *str;       // トークン文字列
+    int len;         // トークンの長さ
+};
+
+typedef enum {
+    ND_ADD,  // +
+    ND_SUB,  // -
+    ND_MUL,  // *
+    ND_DIV,  // /
+    ND_EQ,   // ==
+    ND_NE,   // !=
+    ND_LT,   // <
+    ND_LE,   // <=
+    ND_NUM,  // 整数
+} NodeKind;
+
+// 抽象構文木のノードの型
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // ノードの型
+    Node *lhs;      // 左辺
+    Node *rhs;      // 右辺
+    int val;        // kindがND_NUMの場合のみ使う
+};
+
+// 現在着目しているトークン
+extern Token *token;
+
+// 入力プログラム
+extern char *user_input;
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+bool starts_with(char *p, char *q);
+Token *tokenize(char *p);
+void print_token(const Token *head);
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+void print_node(Node *root);
+
+// codegen.c
+void gen(Node *node);
+
+// main.c
+int main(int argc, char **argv);
